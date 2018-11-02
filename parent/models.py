68c931ab4a491a414ec76f75b07f82d43ec6a265
azamatmurdalov <azamat.murdalov@mail.ru>
2018-11-02 21:56:31 +0300

    create database

--- parent/models.py
+++ parent/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Parent(models.Model):
+	parent_name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.parent_name
+
+class Child(models.Model):
+	parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
+	child_name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.child_name
