63ba1344016949fc244810fc9a5a9a0dc57ae58c
azamatmurdalov <azamat.murdalov@mail.ru>
2018-11-02 22:11:20 +0300

    add parent view

--- parent/views.py
+++ parent/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+from .models import Parent
+
 def index(request):
-	return render(request, 'parent/index.html')
+	latest_parent_list = Parent.objects.order_by('parent_name')
+	context = {'latest_parent_list': latest_parent_list}
+	return render(request, 'parent/index.html', context)
